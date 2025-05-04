fbc00a144290c18a09908882009b881c0fe2ae3a
Anagh Roy <royanagh1999@gmail.com>
2025-05-04 18:55:04 +0530

    complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import Work from "./Components/Work";
 import Stripes from "./Components/Stripes";
@@ -7,8 +7,36 @@ import Marquees from "./Components/Marquees";
 import Cards from "./Components/Cards";
 import Footer from "./Components/Footer";
 import LocomotiveScroll from "locomotive-scroll";
-
+import useDisableInspect from "../disable";
 function App() {
+  useEffect(() => {
+    const handleContextMenu = (e) => e.preventDefault();
+    document.addEventListener("contextmenu", handleContextMenu);
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (
+        e.key === "F12" ||
+        (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
+        (e.ctrlKey && e.key === "U")
+      ) {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  // useDisableInspect.js
+
+  useDisableInspect();
+
   const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className='w-full bg-zinc-900 text-white font-["satoshi"]'>
