d3c0e6e5f2dc7e5ef6891640b1dfebb6ed8bde59
Anagh Roy <royanagh1999@gmail.com>
2025-05-04 20:18:22 +0530

    Fix: added base for GitHub Pages

--- disable.jsx
+++ disable.jsx
@@ -1,23 +1,23 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-export default function useDisableInspect() {
-  useEffect(() => {
-    const blockContextMenu = (e) => e.preventDefault();
-    const blockKeyPress = (e) => {
-      if (
-        e.key === "F12" ||
-        (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
-        (e.ctrlKey && e.key === "U")
-      ) {
-        e.preventDefault();
-      }
-    };
-    document.addEventListener("contextmenu", blockContextMenu);
-    document.addEventListener("keydown", blockKeyPress);
+// export default function useDisableInspect() {
+//   useEffect(() => {
+//     const blockContextMenu = (e) => e.preventDefault();
+//     const blockKeyPress = (e) => {
+//       if (
+//         e.key === "F12" ||
+//         (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
+//         (e.ctrlKey && e.key === "U")
+//       ) {
+//         e.preventDefault();
+//       }
+//     };
+//     document.addEventListener("contextmenu", blockContextMenu);
+//     document.addEventListener("keydown", blockKeyPress);
 
-    return () => {
-      document.removeEventListener("contextmenu", blockContextMenu);
-      document.removeEventListener("keydown", blockKeyPress);
-    };
-  }, []);
-}
+//     return () => {
+//       document.removeEventListener("contextmenu", blockContextMenu);
+//       document.removeEventListener("keydown", blockKeyPress);
+//     };
+//   }, []);
+// }
